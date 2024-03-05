db516cc2b9207ecd4e8f2f4d0d2d63877ac86d84
alejoforeroforero <alejoforeroforero@gmail.com>
2024-03-05 15:07:13 -0500

    cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://visitantesonoro.vercel.app"],
     credentials: true,
   })
 );
