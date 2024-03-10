227ad3b184ff9adb5deca578d77580d9b5e6d974
alejoforeroforero <alejoforeroforero@gmail.com>
2024-03-10 10:35:06 -0500

    celular

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://visitantesonoro.vercel.app"],
+    origin: ["http://localhost:5173", "http://192.168.0.5:5173", "https://visitantesonoro.vercel.app"],
     credentials: true,
   })
 );
